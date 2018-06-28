47a65e040963bc96002d20bc05ca23cf37a1deda
Marta Pryshlyak <pryshlyak@ucu.edu.ua>
2018-06-28 03:16:24 +0300

    Create student.sql

---
+++ ucubot/Scripts/student.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Student (
+     id INT NOT NULL AUTO_INCREMENT,
+     firstname VARCHAR(128) NOT NULL,
+     lastname VARCHAR(128) NOT NULL,
+     user_id VARCHAR(128) NOT NULL UNIQUE,
+     PRIMARY KEY (id)
+);
