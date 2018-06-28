d181b3b8623aa0b63a5c09e6df60c8b32ec034b9
Marta Pryshlyak <pryshlyak@ucu.edu.ua>
2018-06-28 03:18:31 +0300

    Create Student.cs

---
+++ ucubot/Model/Student.cs
@@ -0,0 +1,10 @@
+namespace ucubot.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserId { get; set; }
+    }
+}
