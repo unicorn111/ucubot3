ad0dc0535bd578d4c92c3f71c8bb822cab832ee1
Marta Pryshlyak <pryshlyak@ucu.edu.ua>
2018-06-28 03:04:07 +0300

    Create Connection.cs

---
+++ usubot.End2EndTests/Connection.cs
@@ -0,0 +1,8 @@
+namespace usubot.End2EndTests
+{
+    public class Connection
+    {
+        public const string CONNECTION_STRING_NODB = "Server=db;Uid=root;Pwd=1qaz2wsx";
+        public const string CONNECTION_STRING = "Server=db;Database=ucubot;Uid=root;Pwd=1qaz2wsx";
+    }
+}
